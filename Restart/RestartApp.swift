c58c62500f776a9311524b97530b87f3afd9b564
chizakasipascal <pascalkasichiza@gmail.com>
2023-01-16 21:49:37 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by CHIZA KASI Pascal on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
